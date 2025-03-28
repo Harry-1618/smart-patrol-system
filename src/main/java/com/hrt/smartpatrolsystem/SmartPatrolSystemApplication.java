2ba4d1ff392902c3819e522cb690f1b13cfb244c
hrt <1776463307@qq.com>
2025-03-28 17:19:03 +0800

    整合mybatisPlus

--- src/main/java/com/hrt/smartpatrolsystem/SmartPatrolSystemApplication.java
+++ src/main/java/com/hrt/smartpatrolsystem/SmartPatrolSystemApplication.java
@@ -1,9 +1,11 @@
 package com.hrt.smartpatrolsystem;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hrt.smartpatrolsystem.mapper")
 public class SmartPatrolSystemApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/hrt/smartpatrolsystem/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.hrt.smartpatrolsystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName: UserController
+ * Package: com.hrt.smartpatrolsystem.controller
+ * Description:
+ *
+ * @Author: 黄瑞天
+ * @Create: 2025/3/28 - 17:13
+ * @Version: v1.0
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+}
