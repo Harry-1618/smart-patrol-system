8a177c2a8ce28c5b0ee2d231625c9f6a56897298
hrt <1776463307@qq.com>
2025-03-28 17:01:10 +0800

    智能巡更系统配置搭建

---
+++ src/main/java/com/hrt/smartpatrolsystem/SmartPatrolSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hrt.smartpatrolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPatrolSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartPatrolSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hrt/smartpatrolsystem/SmartPatrolSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrt.smartpatrolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPatrolSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
