de11fc7d9d4f7611e9e8e0f1b3bbf8b5acb22c61
hrt <1776463307@qq.com>
2025-04-04 10:50:53 +0800

    解决了ReportTypeVO数据返回时无法序列化的问题

--- src/main/java/com/hrt/smartpatrolsystem/controller/ReportController.java
+++ src/main/java/com/hrt/smartpatrolsystem/controller/ReportController.java
@@ -2,6 +2,7 @@ package com.hrt.smartpatrolsystem.controller;
 
 import com.hrt.smartpatrolsystem.common.vos.ResponseResult;
 import com.hrt.smartpatrolsystem.service.IReportTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ReportController {
+    @Autowired
     private IReportTypeService reportTypeService;
 
-    @GetMapping("/report-type")
+    @GetMapping("/report-types")
     ResponseResult getReportType(){
         return reportTypeService.getReportTypeList();
     }
--- src/main/java/com/hrt/smartpatrolsystem/report/vos/ReportTypeVO.java
+++ src/main/java/com/hrt/smartpatrolsystem/report/vos/ReportTypeVO.java
@@ -1,5 +1,9 @@
 package com.hrt.smartpatrolsystem.report.vos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
 import java.util.Date;
 
 /**
@@ -11,9 +15,15 @@ import java.util.Date;
  * @Create: 2025/4/3 - 15:25
  * @Version: v1.0
  */
+@Data
 public class ReportTypeVO {
+    @JsonProperty("id")
     private Integer id;
+    @JsonProperty("type_name")
     private String typeName;
+    @JsonProperty("remark")
     private String remark;
+    @JsonProperty("created_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createdTime;
 }
